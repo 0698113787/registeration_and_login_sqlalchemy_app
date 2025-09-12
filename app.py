b6e597c6808188efb62e9c53577685b333568c9a
Andile Mgazi <vuyiswaandile176@gmail.com>
2025-09-13 01:44:28 +0200

    updating port for binfing

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ def login():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 4000))
     app.run(host='0.0.0.0', port=port, debug=False)
