97fa9b0b27c4741a9dc64e6755e03930f299a5a0
Andile Mgazi <vuyiswaandile176@gmail.com>
2025-09-13 18:55:03 +0200

    Mgazi app update

--- app.py
+++ app.py
@@ -109,6 +109,5 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get('PORT', 4000))
     app.run(host='0.0.0.0', port=port, debug=True)  # Set debug=True for development
